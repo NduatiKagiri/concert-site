ed72bebc2b34709c2b8be95acbf0cd0e6719397b
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-05-18 23:33:06 +0300

    Fix Linters errors

--- assets/js/speakers.js
+++ assets/js/speakers.js
@@ -35,8 +35,7 @@ const speakers = [
     title: 'Jamaican Reggae Musician',
     message: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
   },
-]
-
+];
 
 function displaySpeakers() {
   speakers.forEach((speaker) => {
